16245bb3952e5658e9741abf87114d92cbbc37d4
reiitama <rhwijaya15@gmail.com>
2023-08-05 03:59:49 +0700

    First Commit

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include "include/penjualan.php"
 <html>
 <head>
 <title>APLIKASI PENJUALAN</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
@@ -12,7 +13,7 @@ include "include/penjualan.php"
 		<header>
 			<h1>Aplikasi Penjualan </h1>
 		</header>
-		<nav>
+		<nav class="navbar navbar-dark bg-dark">
 			<ul class="menu">
 				<li class="menu-item">
 					<a href="index.php" class="<?php echo !isset($_GET['page'])? "active":"" ?>">Home</a>
@@ -62,5 +63,8 @@ include "include/penjualan.php"
 	?>
 		</section>
 </div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
